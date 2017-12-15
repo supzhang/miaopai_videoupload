46ba374314972130b637214ccde878c24516b05f
supzhang <20zzy@163.com>
2017-12-15 13:19:48 +0800

    增加好看视频的抓取功能

--- videos_get/toutiao/ttspider.py
+++ videos_get/toutiao/ttspider.py
@@ -1,6 +1,5 @@
 #-*- coding:utf-8 -*-
 #用于获取头条的视频信息
-#https://github.com/liubo0621/headlines_today
 import requests
 import re
 import time
@@ -9,10 +8,6 @@ import binascii
 import base64
 from PyQt5.QtCore import QThread,pyqtSignal
 
-#mediaurl = https://www.ixigua.com/a6464842031897248269/#mid=58363379363
-#https://www.ixigua.com/group/6483718332703834638/
-#https://www.ixigua.com/c/user/58363379363/
-#https://www.ixigua.com/item/6497799282349834765/
 class ttspider(QThread):
     finalInfo = pyqtSignal(dict)
     def __init__(self,category,maxcount):  #需要获取的分类
@@ -55,7 +50,6 @@ class ttspider(QThread):
         except Exception as e:
             print(e)
     def processData(self):
-        #print(len(self.rett))
         try:
             abstract=d['abstract']
         except Exception as e:
@@ -78,14 +72,13 @@ class ttspider(QThread):
                 self.ret = final
                 print(final)
                 self.finalInfo.emit(final)
-               # print('aaaaaaaaaa',final)
+
 
             except Exception as e:
                 print(e)
 
     def get_video_url(self,info):
         r = str(random.random())[2:]
-        #r = '28753395491314815'
         r_1 = ('/video/urls/v/1/toutiao/mp4/' + info['video_id'] + '?r=' + r)
         r_bin  =r_1.encode('utf-8')
         s = binascii.crc32(r_bin)
@@ -100,7 +93,6 @@ class ttspider(QThread):
             print('获取视频真实地址：',e)
         return info
     def get_url_real_url(self,url):   #获取真实视频地址
-        print('-----------------------',url)
         res = requests.get(url,headers = self.headers,verify = False)
         url_json = res.json()
         videolist = url_json['data']['video_list']
@@ -120,34 +112,12 @@ class ttspider(QThread):
         return val >> n if val >= 0 else (val + 0x100000000) >> n
 
 
-
-# t = ttspider('subv_entertainment',5)
-#
-# t.start()
-# t.exec()
+t = ttspider('subv_funny',20)
+t.start()
+t.exec()
 
 
 
-# from urllib.parse import urlparse
-# import binascii
-#
-# def right_shift(val, n):
-#     return val >> n if val >= 0 else (val + 0x100000000) >> n
-# import random
-# r = str(random.random())[2:]
-# vid = '185e6cd90312486dbd4a99ddf66dd4c1'
-# url = 'https://ib.365yg.com/video/urls/v/1/toutiao/mp4/%s' % vid
-# ###https://ib.365yg.com/video/urls/v/1/toutiao/mp4/4d0b1c54cead42e4ab56c1ea821c2a39?r=27247661144819957&s=805232848&callback=tt_playerooswk
-# r = '27247661144819957'
-# n = urlparse(url).path + '?r=' + r
-# n = n.encode('utf-8')
-# print(n)
-# c = binascii.crc32(n)
-# s = right_shift(c, 0)
-# print('-------------',c)
-# url = 'https://ib.365yg.com/video/urls/v/1/toutiao/mp4/c0d0bbbcc22d48b4ae4327a35198a184?r=%s&s=%s'%(r,s)
-# res = requests.get(url)
-# print(res.json())
 # categories = {'搞笑':'subv_funny',
 #               '音乐':'subv_voice',
 #               '推荐':'video_new',
