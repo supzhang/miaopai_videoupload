a8406994867c8719bd8bd5a29ce913a1f9bee9b1
supzhang <20zzy@163.com>
2017-12-15 17:34:16 +0800

    增加好看视频的抓取功能

---
+++ backup/test.js
@@ -0,0 +1,34 @@
+/**
+ * Created by zzy on 2017/12/15.
+ */
+
+lib.js
+
+ bhash: s(this.result)
+/** 获取n值 函数*/
+function n(e) {
+            e ? (a[0] = a[16] = a[1] = a[2] = a[3] = a[4] = a[5] = a[6] = a[7] = a[8] = a[9] = a[10] = a[11] = a[12] = a[13] = a[14] = a[15] = 0,
+            this.blocks = a) : this.blocks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            this.h0 = 1732584193,
+            this.h1 = 4023233417,
+            this.h2 = 2562383102,
+            this.h3 = 271733878,
+            this.h4 = 3285377520,
+            this.block = this.start = this.bytes = 0,
+            this.finalized = this.hashed = !1,
+            this.first = !0
+        }
+
+
+
+        http://up-bjyt.oss.yunpan.360.cn/Object.block/vod-shouzhu-bj/Mjk3NjIxMzkwNV8yMDE3MTIxNTEzMTM0NF9NekxwbUwvbWxxL29rb0poWW1Qb2lxd2c1WVdMNXA2WDZaeUo1N1NnSU9hY3JDQW9NaWt1YlhBMC5tcDQ=?tid=151331482420873029&bid=0&uid=2681611121&scid=46&
+
+s=782430ba07760b8638e4ec76b6522c54&id=WU_FILE_0&name= 文件名 &type=video%2Fmp4&lastModifiedDate=文件修改时间戳&size=文件字节数&bhash=979ce3091d1ab5adfd72ceba275ef63366efcedd&chunks=46&chunk=0
+
+http://up-bjyt.oss.yunpan.360.cn/Object.commit/vod-shouzhu-bj/Mjk3NjIxMzkwNV8yMDE3MTIxNTEzMTM0NF9NekxwbUwvbWxxL29rb0poWW1Qb2lxd2c1WVdMNXA2WDZaeUo1N1NnSU9hY3JDQW9NaWt1YlhBMC5tcDQ=?tid=151331482420873029&uid=2681611121&scid=46
+
+&s=db46b91d8c7907f4c86e3a4c9a5ede40
+
+
+s 14165
+bash 14785
